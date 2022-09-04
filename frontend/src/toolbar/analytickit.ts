33e6c038eb14df5aa9ddfb45a87b09ee4994dcf0
Mani Malarvannan <manivannan.malarvannan@xcelenergy.com>
2022-09-04 16:27:13 -0500

    node pacage error fixed

--- frontend/src/toolbar/analytickit.ts
+++ frontend/src/toolbar/analytickit.ts
@@ -1,16 +1,16 @@
 import AnalyticKit from 'analytickit-js-lite'
 
-const runningOnAnalyticKit = !!window.analytickit_APP_CONTEXT
-const apiKey = runningOnAnalyticKit ? window.JS_analytickit_API_KEY : 'sTMFPsFhdP1Ssg'
-const apiHost = runningOnAnalyticKit ? window.JS_analytickit_HOST : 'https://app.analytickit.com'
+const runningOnAnalytickit = !!window.analytickit_APP_CONTEXT
+const apiKey = runningOnAnalytickit ? window.JS_ANALYTICKIT_API_KEY : 'sTMFPsFhdP1Ssg'
+const apiHost = runningOnAnalytickit ? window.JS_ANALYTICKIT_HOST : 'https://app.analytickit.com'
 
 export const analytickit = new AnalyticKit(apiKey, {
-host: apiHost,
-enable: false, // must call.optIn() before any events are sent
+    host: apiHost,
+    enable: false, // must call.optIn() before any events are sent
     persistence: 'memory', // We don't want to persist anything, all events are in-memory
     persistence_name: apiKey + '_toolbar', // We don't need this but it ensures we don't accidentally mess with the standard persistence
 })
 
-if (runningOnanalytickit && window.JS_analytickit_SELF_CAPTURE) {
+if (runningOnAnalytickit && window.JS_ANALYTICKIT_SELF_CAPTURE) {
     analytickit.debug()
 }

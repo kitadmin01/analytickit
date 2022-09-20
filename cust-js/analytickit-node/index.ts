67152e69be2062da219e58a28a94abfc2a2371c8
Mani Malarvannan <manivannan.malarvannan@xcelenergy.com>
2022-09-20 16:25:36 -0500

    node packages added to cust-js

---
+++ cust-js/analytickit-node/index.ts
@@ -0,0 +1,4 @@
+import { AnalyticKitGlobal } from './src/analytickit-node'
+export * from './src/analytickit-node'
+
+export default AnalyticKitGlobal
---
+++ cust-js/analytickit-node/lib/analytickit-node/index.d.ts
@@ -0,0 +1,3 @@
+import { AnalyticKitGlobal } from './src/analytickit-node';
+export * from './src/analytickit-node';
+export default AnalyticKitGlobal;

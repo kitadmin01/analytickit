d2858f16be94388797e5ed94279c62d47fa8db82
Mani Malarvannan <manivannan.malarvannan@xcelenergy.com>
2023-09-20 20:56:50 -0500

    mmdb issue fixed

--- analytickit/crypto/txn_anz.py
+++ analytickit/crypto/txn_anz.py
@@ -15,15 +15,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'analytickit.settings')
 django.setup()
 
 
-import structlog
+from logger_config import logger
 import datetime
 from analytickit.models.crypto.comm_eng import CommunityEngagement,CampaignAnalytic,S3File
 from analytickit.crypto.s3_ret import S3Retriever
 from analytickit.crypto.metric_cal import MetricCalculator 
 from django.db import transaction, IntegrityError
-
-
-logger = structlog.get_logger(__name__)
+import gc
 
 
 class TxnAnalyzer:
@@ -33,6 +31,7 @@ class TxnAnalyzer:
         self.config = config
         self.s3_retriever = S3Retriever(config)
         logger.info("created TxnAnalyzer")
+
 
 
     def perform_campaign_analysis(self):
@@ -47,24 +46,35 @@ class TxnAnalyzer:
         """
         com_engs = CommunityEngagement.get_campaign_records_today()
 
-        logger.info('total comengs=',com_engs[0])
         s3_keys =  S3File.get_all_key_names()
         # filtered_keys used for testing only
-        filtered_keys = [
+        '''filtered_keys = [
             key for key in s3_keys 
-            if datetime.strptime(key.split('date=')[1].split('/')[0], "%Y-%m-%d") >= datetime(2023, 8, 27)
+            if datetime.datetime.strptime(key.split('date=')[1].split('/')[0], "%Y-%m-%d") >= datetime.datetime(2023, 8, 27)
         ]
-        s3_keys = filtered_keys
+        s3_keys = filtered_keys'''
+
+        BATCH_SIZE = 100  # Define batch size
+        s3_keys_batches = [s3_keys[i:i + BATCH_SIZE] for i in range(0, len(s3_keys), BATCH_SIZE)]
 
         for campaign in com_engs:
-            camp_analytic  = CampaignAnalytic.get_or_create_for_today(campaign)
+            if campaign.is_first_campaign_analytic is not True:
+                s3_key_batches = [S3File.get_all_key_names()[i:i + BATCH_SIZE] for i in range(0, len(S3File.get_all_key_names()), BATCH_SIZE)]
+            else:
+                s3_key_batches = [S3File.get_previous_day_key_name()[i:i + BATCH_SIZE] for i in range(0, len(S3File.get_previous_day_key_name()), BATCH_SIZE)]
 
-            eth_jsons = self.s3_retriever.get_blockchain_data(
-                campaign.contract_address, 
-                campaign.token_address, 
-                s3_keys
-            )
-            logger.info("eth_jsons=", eth_jsons)
+            # Now, process each batch of s3 keys separately
+            for s3_key_batch in s3_key_batches:
+                eth_jsons = self.s3_retriever.get_blockchain_data(
+                    campaign.contract_address,
+                    campaign.token_address,
+                    s3_key_batch
+                )        
+            logger.info("logging_event", eth_jsons=eth_jsons)
+
+            # create a new or get existsing camp_analytic for today. Only one
+            # camp_analytic per day.
+            camp_analytic = CampaignAnalytic. get_or_create_for_today(campaign)
                         
             met_cal = MetricCalculator(eth_jsons,campaign.contract_address, campaign.token_address)
             camp_analytic.active_users = met_cal.calculate_dau()
@@ -87,14 +97,17 @@ class TxnAnalyzer:
             camp_analytic.token_transfer_value_distribution = met_cal.calculate_token_transfer_value_distribution()
 
 
-            logger.info("camp_analytic.active_users=",camp_analytic.active_users)
+            logger.info("camp_analytic.active_users",active_users=camp_analytic.active_users)
             camp_analytic.save()
 
+            del eth_jsons  # Delete large variables to free memory
+            gc.collect()   # Force garbage collection
                 
-
     def retrieve_store_all_keys(self):
         """
         Uses the s3_retriever to get all the Ethereum S3 keys since 2020 and then stores them in the DB.
+        This will be run only once to store all the keys until the the day it was run. Next day only the 
+        store_keys_for_next_day fn will be run to store next day key.
         """
         keys = self.s3_retriever.fetch_keys_since_2020()
         for key_name in keys:
@@ -112,7 +125,6 @@ class TxnAnalyzer:
             except Exception as e:
                 logger.error(f"Exception while processing key_name {key_name}: {e}")
 
-
     def store_keys_for_next_day(self):
         """
         Retrieves the latest S3File creation timestamp from the DB, adds one day, 
@@ -120,10 +132,10 @@ class TxnAnalyzer:
         """
         if latest_s3file := S3File.objects.order_by('-creation_ts').first():
             next_date = latest_s3file.creation_ts + datetime.timedelta(days=1)
-            new_files = self.s3_retriever.fetch_files_for_date(next_date.date())
+            new_files = self.s3_retriever.fetch_keys_for_date(next_date.date())
 
-            for file_name in new_files:
-                S3File.objects.get_or_create(file_name=file_name)
+            for key_name in new_files:
+                S3File.objects.get_or_create(key_name=key_name)
 
             return f"{len(new_files)} files fetched and stored for date {next_date.date()}."
 
@@ -139,5 +151,14 @@ class TxnAnalyzer:
 
 if __name__ == "__main__":
     analyzer = TxnAnalyzer("")
-    # analyzer.retrieve_store_all_keys()
+
+    if S3File.any_record_exists() is not True:
+        # if no signle S3File record exist, fetch all the keys from S3
+        
+        analyzer.retrieve_store_all_keys()
+    else:
+        # retrieve and store the key for the next day
+        analyzer.store_keys_for_next_day()
+
+    # start the campaign analysis
     analyzer.perform_campaign_analysis()

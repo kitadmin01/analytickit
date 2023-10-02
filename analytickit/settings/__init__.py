755f3776fc8f68b52ac1477bd9b165b6935ebf6c
Mani Malarvannan <manivannan.malarvannan@xcelenergy.com>
2023-10-02 08:14:27 -0500

    stripe keys removed from setting.py

--- analytickit/settings/__init__.py
+++ analytickit/settings/__init__.py
@@ -133,6 +133,6 @@ if "dpa.apps.EnterpriseConfig" in INSTALLED_APPS:
 from analytickit.settings.cloud import *  # noqa: F401
 
 # Stripe
-STRIPE_PUBLIC_KEY = "pk_test_51MCCGYFtMel7myQSEOfWiBOkj5xiGIBFWurBvRQuk9NmMCl6KyidtoGLobYwWd84ADPNwUBS71VS1GVC7vm9P9Jx00QxCLxtJ4"
-STRIPE_SECRET_KEY = "sk_test_51MCCGYFtMel7myQSX3WLQDtzbA63XRE7ykr8ZhgLP9Ay9ibTw6cPj5v3pSiXEXPwcsh06XR1t8hfd1XVdPX7faLM00QbleRLBs"
-STRIPE_WEBHOOK_SECRET = "whsec_aPIJsiXAApNnsB3ox9EBYfnYoWgv09dm"
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+STRIPE_WEBHOOK_SECRET_KEY = os.environ.get('STRIPE_WEBHOOK_SECRET_KEY')
